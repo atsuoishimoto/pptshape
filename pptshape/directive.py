2a3eab4103e18599e022c8cd3ebd0c68f7d0b825
Atsuo Ishimoto <ishimoto@gembook.org>
2013-03-18 13:01:09 +0900

    added test

--- pptshape/directive.py
+++ pptshape/directive.py
@@ -17,10 +17,17 @@ class PPTShape(images.Image):
             filename = os.path.join(dirname, self.options['pptfilename'])
             shapename = self.options['shapename']
             imagename = os.path.join(dirname, self.arguments[0])
-            pptfile = ppt.open(filename)
-            if pptfile:
-                pptfile.saveShape(shapename, imagename)
-            pptfile.quit()
+
+            # check if ppt was updated.
+            mtime_img = 0
+            if os.path.exists(imagename):
+                mtime_img = os.stat(imagename).st_mtime
+            # TODO: How to emit error message?
+            if os.stat(filename).st_mtime > mtime_img:  
+                pptfile = ppt.open(filename)
+                if pptfile:
+                    pptfile.saveShape(shapename, imagename)
+                pptfile.quit()
 
         return super().run()
 
--- test/test_ppt.py
+++ test/test_ppt.py
@@ -1,5 +1,7 @@
 import os, sys
+from unittest import mock
 import pptshape.ppt
+import pptshape.directive
 
 TESTDIR = os.path.split(__file__)[0]
 
@@ -9,4 +11,35 @@ def test_open():
                 os.path.join(TESTDIR, 'aaa.png'))
     ppt.quit()
 
+def test_update():
+    NAME_PNG = 'abc.png'
+    path_png = os.path.join(TESTDIR, NAME_PNG)
 
+    NAME_PPT = 'testppt.pptx'
+    path_ppt = os.path.join(TESTDIR, NAME_PPT)
+
+    directive = pptshape.directive.PPTShape(None, None, None, 
+             None, None, None, None, None, None)
+    if os.path.exists(path_png):
+        os.unlink(path_png)
+
+    directive.arguments = [NAME_PNG]
+    directive.state = mock.Mock()
+    directive.state.document.current_source = os.path.join(TESTDIR, 'test.rst')
+    directive.options = {
+        'pptfilename': NAME_PPT,
+        'shapename': 'shape-title',
+    }
+    directive.run()
+    assert os.path.exists(path_png)
+    
+    os.utime(path_png, (0, 0))
+    directive.run()
+
+    assert os.stat(path_png).st_mtime != 0
+
+    # Make png file empty.
+    open(path_png, 'w').close()
+
+    directive.run()
+    assert os.path.getsize(path_png) == 0
